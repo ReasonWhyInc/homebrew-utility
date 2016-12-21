120a5e71021192aa78c7d5895dd5ccbae87c8972
Hirotaka Suzuki <suhirotaka@gmail.com>
2016-12-21 15:51:21 +0900

    created Formula

---
+++ formula.rb
@@ -0,0 +1,39 @@
+# Documentation: https://github.com/Homebrew/brew/blob/master/docs/Formula-Cookbook.md
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Ecs < Formula
+  desc "Login to ECS instance or container by ECS service name"
+  homepage "https://github.com/suhirotaka/homebrew-ecs-login"
+  url "https://github.com/suhirotaka/homebrew-ecs-login/raw/master/ecs-login.tar.gz"
+  version "1.0.0"
+  sha256 "d379e4de8a8296d35df4ee69dc347db703ff3273188f341eb7205fb99bcddb37"
+
+  # depends_on "cmake" => :build
+  depends_on :x11 # if your formula requires any X11/XQuartz components
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test ecs`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
