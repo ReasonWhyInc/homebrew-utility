f26f7262f6c56b43788a4289a0f409de1db14efc
Hirotaka Suzuki <suhirotaka@gmail.com>
2016-12-24 11:41:25 +0900

    removed `make`

--- ecslogin.rb
+++ ecslogin.rb
@@ -22,7 +22,7 @@ class Ecslogin < Formula
 #                          "--prefix=#{prefix}"
     # system "cmake", ".", *std_cmake_args
     # system "make", "install" # if this fails, try separate make/make install steps
-    system "make"
+#    system "make"
     bin.install "ecslogin"
   end
 
