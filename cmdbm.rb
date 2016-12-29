c758cb2ba5645087618ab921f8d299f004e420a4
Hirotaka Suzuki <suhirotaka@gmail.com>
2016-12-29 15:31:23 +0900

    changed directory structure

---
+++ cmdbm.rb
@@ -0,0 +1,48 @@
+# Documentation: https://github.com/Homebrew/brew/blob/master/docs/Formula-Cookbook.md
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Cmdbm < Formula
+  desc "Adds command bookmark functionality"
+  homepage "https://github.com/suhirotaka/homebrew-shell-scripts"
+  url "https://github.com/suhirotaka/homebrew-shell-scripts/raw/master/command_bookmarks.sh.tar.gz"
+  version "1.0.0"
+  sha256 "39cb3011ba7a2f4b4a6d03154f70e62002479da7b3531cd9001ae845ea6d6af6"
+
+  # depends_on "cmake" => :build
+  # depends_on :x11 # if your formula requires any X11/XQuartz components
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+
+    # Remove unrecognized options if warned by configure
+    # system "./configure", "--disable-debug",
+    #                       "--disable-dependency-tracking",
+    #                       "--disable-silent-rules",
+    #                       "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    # system "make", "install" # if this fails, try separate make/make install steps
+    system "mv", "command_bookmarks.sh", "cmdbm"
+    bin.install "cmdbm"
+  end
+
+  def caveats
+    msg = <<-EOF.undent
+cmdbm successfully installed.
+EOF
+    msg
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test ecs`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+     system "false"
+  end
+end
---
+++ shell/command_bookmarks.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+BM_FILENAME=~/.command_bookmarks
+VERSION_CODE=1.0.0
+
+# "Add" option
+case "$1" in
+  "add" )
+    if [ -n "$2" ]; then
+      echo "${@:2}" >> $BM_FILENAME
+      echo "Added a new command bookmark."
+    else
+      echo "Please specify a command to be added."
+      exit 1
+    fi
+    ;;
+
+# "List" option
+  "ls" )
+    line_num=1
+    cat $BM_FILENAME | while read line
+    do
+      echo $line_num: $line
+      line_num=$(($line_num + 1))
+    done
+    ;;
+
+# "Edit" option
+  "edit" )
+    "${EDITOR:-vi}" $BM_FILENAME
+    ;;
+
+# "Remove" option
+  "rm" )
+    if [[ ! "$2" =~ ^[0-9]+$ ]]; then
+      echo "Invalid command number specified."
+      exit 1
+    fi
+    sed -i -e "${2},${2}d" $BM_FILENAME
+    ;;
+
+# "Run" option
+  "run" )
+    if [[ ! "$2" =~ ^[0-9]+$ ]]; then
+      echo "Invalid command number specified."
+      exit 1
+    fi
+    run_command=`sed -n ${2}p $BM_FILENAME`
+    if [[ -z "$run_command" ]]; then
+      echo "Command not found."
+      exit 1
+    fi
+    echo "$(basename $0): Running \`$run_command\`"
+    eval $run_command
+    ;;
+
+# "--help" option
+  "--help" )
+    cat <<__EOS__
+$(basename $0) is a tool for command bookmarks
+
+Usage: $(basename $0) [<action>] [<options>]
+
+Actions:
+   add       Add a bookmark
+   ls        List bookmarks
+   edit      Edit bookmarks
+   rm        Delete a bookmark
+   run       Run a bookmarked command
+
+Options:
+  --help     Print this
+  --version  Show version
+__EOS__
+    ;;
+
+# "--version" option
+  "--version" )
+    echo "$(basename $0) version $VERSION_CODE"
+    ;;
+
+# other actions
+  * )
+    echo "Unknown action specified."
+    exit 1
+    ;;
+esac
